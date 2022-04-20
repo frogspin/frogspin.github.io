f6fcc2db5fcfd636259bbeab755b90db6326fb6a
frogspin <104053744+frogspin@users.noreply.github.com>
2022-04-20 02:53:50 -0400

    Update duck.js

--- duck.js
+++ duck.js
@@ -1,4 +1,24 @@
-const easterEgg = new Konami("https://frogspin.github.io/duck.html")
+const duck = new Konami("duck.html")
+const frog = new Konami("phrog.html")
 
-easterEgg.pattern = "68856775"
+duck.pattern = "68856775"
+frog.pattern = "70827971"
 
+var shown = 1;
+function showfps() {
+	if (shown == 0) {
+		console.log(shown)
+		document.getElementById('fpsCount').style.color = 'black'
+		shown = 1
+	} else if (shown == 1) {
+		console.log(shown)
+		document.getElementById('fpsCount').style.color = 'white'
+		shown = 0
+	}
+
+}
+
+const showFps = new Konami(() => {
+	const show = showfps()
+})
+showFps.pattern = "83727987"
